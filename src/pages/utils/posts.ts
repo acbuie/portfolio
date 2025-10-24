f8d75e6da278dfb6ce22fa06d63b5b48c16ecd02
acbuie <aidancbuie@gmail.com>
2025-10-24 15:54:59 -0600

    refactor: Move post sorting logic to utils

---
+++ src/pages/utils/posts.ts
@@ -0,0 +1,29 @@
+import type { CollectionEntry } from "astro:content";
+
+export function arrangePostCollection(
+  rawPosts: CollectionEntry<"blogPosts">[],
+) {
+  const posts = rawPosts.reduce(
+    (groups: { [key: string]: CollectionEntry<"blogPosts">[] }, post) => {
+      const year = post.data.pubDate.getFullYear();
+      if (!groups[year]) {
+        groups[year] = [];
+      }
+      groups[year].push(post);
+      return groups;
+    },
+    {},
+  );
+
+  // Convert to array of objs, sorting by date
+  return Object.keys(posts)
+    .map((year) => {
+      return {
+        year,
+        posts: posts[year].sort(
+          (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
+        ),
+      };
+    })
+    .sort((a, b) => parseInt(b.year) - parseInt(a.year));
+}
