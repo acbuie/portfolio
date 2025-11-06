964b64923f4e4799cfdddb6ece4af56bbc09d5f6
acbuie <aidancbuie@gmail.com>
2025-11-06 13:44:03 -0700

    feat: Tweak Aside, increase code block rounding

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
       themes: ["gruvbox-dark-soft", "gruvbox-light-soft"],
       styleOverrides: {
         borderWidth: "0px",
-        borderRadius: "0.25rem",
+        borderRadius: "0.75rem",
       },
     }),
     mdx(),
