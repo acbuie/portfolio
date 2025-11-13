f9fc2eb04fbee1f89f5786a9358c0ed9cd4e8d48
Aidan Buie <aidancbuie@gmail.com>
2025-11-13 11:31:09 -0700

    feat: Add blog components (#18)
    * refactor: Add PostMeta component
    
    * refactor: Remove Post layout, move to posts slug
    
    * refactor: Move article title to PostMeta
    
    * refactor: Adjust padding for small screens
    
    * feat: Remove typography plugin, scaffold styling
    
    * fix: Remove empty div when no updateDate
    
    * feat: Remove code block border
    
    * feat: Apply typography-like styles to md
    
    * chore: Uninstall tailwind typography
    
    * feat: Add Aside component
    
    * feat: Make styles consistent b/w Aside & code
    
    * feat: Tweak Aside, increase code block rounding
    
    * fix: Correctly apply code theme on page load
    
    * fix: Distinguish code block header
    
    * feat: Increase Aside Y padding
    
    * feat: Apply margin to Aside <p>
    
    * feat: Add astro-icon
    
    * refactor: Use svg icons instead of fontawesome
    
    * fix: Fix comment typo
    
    * feat: Apply margin to code blocks
    
    * fix: Fix border color for terminal frames
    
    * feat: Implement Step component
    
    Wrap <Step> in <ol class="step> in mdx to use
    
    * fix: Correctly ignore ol.step lists
    
    * chore: Clean up packages
    
    * feat: Add FileTree component from Starlight
    
    * fix: Fix margin on Steps & FileTree when combined

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://aidanbuie.com",
@@ -19,7 +21,16 @@ export default defineConfig({
   integrations: [
     expressiveCode({
       themes: ["gruvbox-dark-soft", "gruvbox-light-soft"],
+      styleOverrides: {
+        frames: {
+          terminalTitlebarBorderBottomColor: ({ theme }) =>
+            theme.colors["editorGroupHeader.tabsBorder"],
+        },
+        borderRadius: "0.75rem",
+        borderColor: ({ theme }) => theme.colors["editor.background"],
+      },
     }),
     mdx(),
+    icon(),
   ],
 });
