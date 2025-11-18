1618390bcc9b69d170fa790a8858d82baa5d3234
Aidan Buie <aidancbuie@gmail.com>
2025-11-18 13:00:32 -0700

    feat: Add rehype-links plugins (#20)
    * chore: Remove unused styles
    
    * feat: Add external link plugin
    
    * feat: Add icons to headers and external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 
 import icon from "astro-icon";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+
+import { ExternalLinkIcon } from "./src/icons/externalLink";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +21,46 @@ export default defineConfig({
 
   redirects: {
     "/posts": "/blog",
+  },
+
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          content: {
+            type: "text",
+            value: "#",
+          },
+          headingProperties: {
+            className: [
+              "[&>a.anchor-tag]:hidden hover:[&>a.anchor-tag]:inline",
+            ],
+          },
+          properties: {
+            className: [
+              "anchor-tag ml-2 text-secondary no-underline hover:underline",
+            ],
+          },
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          content: () => [ExternalLinkIcon],
+          // target: "_blank",
+          rel: "noopener noreferrer nofollow",
+          properties: {
+            className: "external",
+          },
+          contentProperties: {
+            className: "size-4 inline-block",
+          },
+        },
+      ],
+    ],
   },
 
   integrations: [
@@ -30,6 +75,7 @@ export default defineConfig({
         borderColor: ({ theme }) => theme.colors["editor.background"],
       },
     }),
+
     mdx(),
     icon(),
   ],
